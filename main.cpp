dd53b4fce07a3523ef244939406f7ecb5c9b4031
jzheadley <headleyjz@vcu.edu>
2018-09-18 22:18:46 -0400

    finally predicting something other than 0

--- main.cpp
+++ main.cpp
@@ -18,53 +18,32 @@ double euclideanDistance(ArffInstance* instance1, ArffInstance* instance2, int n
     return sqrt(sum);
 }
 
-int* NN(ArffData* dataset)
-{
-    int* predictions = (int*)malloc(dataset->num_instances() * sizeof(int));
-    printf("The number of instances is %d\n", dataset->num_instances());
-    printf("The number of attributes is %d\n", dataset->num_attributes());
-    for (int instanceIndex=0; instanceIndex < dataset->num_instances(); instanceIndex++){
-        double bestDistance=FLT_MAX;
-        ArffInstance* nearestNeighbor;
-        for (int instance2Index=0; instance2Index < dataset->num_instances(); instance2Index++){
-            if (instanceIndex == instance2Index){
-                continue;
-            }
-            double distance = euclideanDistance(dataset->get_instance(instanceIndex),dataset->get_instance(instance2Index),dataset->num_attributes()-1);
-            // We've found a closer neighbor so lets record
-            if(distance < bestDistance){
-                bestDistance = distance;
-                nearestNeighbor = dataset->get_instance(instance2Index);
-            }
-        }
-        int prediction = nearestNeighbor->get(dataset->num_attributes()-1)->operator int32();
-        // Put class prediction into array
-        predictions[instanceIndex] = prediction;
-        int classValue =  dataset->get_instance(instanceIndex)->get(dataset->num_attributes() - 1)->operator int32();
-    }
-    
-    return predictions;
-}
 typedef struct
 {
     ArffInstance*   neighbor;
     double          distance;
 } NeighborDistance;
 
-int isCloser(NeighborDistance* nearestNeighbors, double newNeighborDistance, int numNearestNeighbors)
-{
-    for (int i = 0; i < numNearestNeighbors; ++i)
+int vote(NeighborDistance* nearestNeighbors, int k, int numAttributes){
+    int* classVotes = (int *)malloc(numAttributes * sizeof(int));
+    for (int i = 0; i < k; i++)
     {
-        if (nearestNeighbors[i].neighbor == NULL)
+        int classVote = nearestNeighbors[i].neighbor->get(numAttributes-1)->operator int32();
+        // if(classVotes[classVote] == NULL)
+        //     classVotes[classVote] = 0;
+        // else
+            classVotes[classVote]++;
+    }
+    int indexOfMax = 0;
+    for (int i = 0; i < numAttributes; i++)
+    {
+        if (classVotes[indexOfMax] < classVotes[i])
         {
-            return i;
-        }
-        else if (newNeighborDistance < nearestNeighbors[i].distance)
-        {
-            return i;
+            indexOfMax = i;
         }
     }
-    return -1;
+    // printf("predictedClass: %i\n",indexOfMax);
+    return indexOfMax;
 }
 
 int* KNN(ArffData* dataset, int k)
@@ -72,23 +51,47 @@ int* KNN(ArffData* dataset, int k)
     int* predictions = (int *) malloc(dataset->num_instances() * sizeof(int));
     int numInstances =  dataset->num_instances();
     int numAttributes =  dataset->num_attributes();
-    NeighborDistance* nearestNeighbors = (NeighborDistance*) malloc(k * sizeof(NeighborDistance));
      for (int instanceIndex=0; instanceIndex < numInstances; instanceIndex++)
      {
         ArffInstance* instance1 = dataset->get_instance(instanceIndex);
+        NeighborDistance* nearestNeighbors = (NeighborDistance*) malloc(k * sizeof(NeighborDistance));
+        double worstBestDistance = FLT_MAX;
         for (int instance2Index=0; instance2Index < numInstances; instance2Index++)
         {
             if (instanceIndex == instance2Index)
                 continue;
             double newNeighborDistance = euclideanDistance(instance1, dataset->get_instance(instance2Index), numAttributes);
-            int closer = isCloser(nearestNeighbors, newNeighborDistance, k);
-            if (closer != -1)
+            
+            // lets handle first inputs a little differently so I don't have to check for nulls later
+            /*if (instance2Index < k)
             {
-                nearestNeighbors[closer].distance = newNeighborDistance;
-                nearestNeighbors[closer].neighbor = dataset->get_instance(instance2Index);
-            }
-            predictions[instanceIndex] = nearestNeighbors[0].neighbor->get(numAttributes-1)->operator int32();
+                nearestNeighbors[instance2Index].distance = newNeighborDistance;
+                nearestNeighbors[instance2Index].neighbor = dataset->get_instance(instance2Index);
+                if (newNeighborDistance < worstBestDistance)
+                {
+                    worstBestDistance = newNeighborDistance;   
+                }
+            } else {*/
+                int indexOfMax = 0;
+                for(int i=0; i < k; i++)
+                {
+                    if (nearestNeighbors[i].neighbor == NULL)
+                    {
+                        indexOfMax = i;
+                        break;
+                    }
+                    else if (nearestNeighbors[indexOfMax].distance < nearestNeighbors[i].distance)
+                    {
+                        indexOfMax=i;
+                    }
+                }
+
+                nearestNeighbors[indexOfMax].distance = newNeighborDistance;
+                nearestNeighbors[indexOfMax].neighbor = dataset->get_instance(instance2Index);
+            // }
+
         }
+        predictions[instanceIndex] = vote(nearestNeighbors, k, numAttributes);
      }
 
     return predictions;
